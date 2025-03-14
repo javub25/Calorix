7a83611df241a4b8803a2753ddcfc9154d70475f
javub25 <ju231415sad@gmail.com>
2025-03-14 13:01:55 +0100

    refactor(pie): move sport metrics and update pie to new directory

--- src/features/chart/utils/getSportMetrics.ts
+++
@@ -1,8 +0,0 @@
-import { PieData } from "@/features/chart/types/Pie";
-
-export const getSportMetrics = ({sportName, data}: {sportName: string, data: PieData}) => 
-{
-    const metrics = data.find(element => element.sport === sportName);    
-
-    return {metrics};
-}
